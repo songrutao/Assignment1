85dc53258c589fb3adca1f18c1b68567706d5008
Rutao.Song.6972 <1214007242@qq.com>
2019-10-07 11:22:01 -0700

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,34 @@
+class car:
+    def __init__(self, interior, transmission, chassis, motor):
+        self.interior = interior
+        self.transmission = transmission
+        self.chassis = chassis
+        self.motor = motor
+
+    def interior (self, input):
+        self.interior = input
+        print ("Color availabilaty: "% + self.interior)
+
+    def transmission (self, input):
+        self.transmission = input
+        print ("transmission in this vehicle: "% + self.transmission)
+
+    def chassis (self, input):
+        self.chassis = input
+      #  print "type of the chassis is: " self.chassis
+
+    def motor (self, input):
+        self.motor = input
+    #    print "the motor is: " self.motor
+
+Car1 = car("carbonfiber", 7, "a", "ss" )
+
+Car1.interior()
+
+#Car1.transmission()
+
+#def main():
+#    while (True):
+#        selection = input("which car do you want to take a look( 1 - 3), otherwise exit this program (4)")
+#        if (selection == 1)
+
